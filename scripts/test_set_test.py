a41e9e045a967bef8265b60882c352b820ccc72e
ov0392@kit.edu <ov0392@fh2n1921.localdomain>
2019-01-13 17:34:04 +0100

    with this file to test the test set with 1000 epochs model, but results seem not to be right, wish the file to be corrected.

---
+++ scripts/test_set_test.py
@@ -0,0 +1,74 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from torchfold import Fold
+
+from sklearn.utils import shuffle
+from sklearn.metrics import accuracy_score
+
+import numpy as np
+
+import pytreebank
+
+import re
+import time
+
+from RNTN_v2 import *
+import pandas as pd
+
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print('Training on {}'.format(device))
+
+# in[ ]:
+net = RNTN(len(word2idx), d=30)
+net.to(device)
+loss_f = nn.NLLLoss() # Negative log likelihood loss
+optimizer = optim.Adagrad(net.parameters(), weight_decay=0.001) # Adagrad with L2 regularization
+
+
+# In[23]:
+
+
+## Test on the test Set
+
+with torch.no_grad():
+    accuracyAll = []
+    accuracyRoot = []
+    res_file = pd.read_csv('../data/accuracyComp.csv')
+    accuracyAll.extend(res_file.values[0])
+    accuracyRoot.extend(res_file.values[1])
+
+    ## FineAll
+    checkpointFineAll = torch.load('../savedModels/d30/fineAll/net_1000.pth')
+    net.load_state_dict(checkpointFineAll)
+    net.eval()
+
+    res = allOutput(bank['test'], net, device)
+    accuracyAll.append(accuracy_score(torch.argmax(res[0], dim=1), res[1]))
+    print('FineAll: Phrase Accuracy on test set = {}'.format(accuracyAll[-1]))
+
+    res = rootOutput(bank['test'], net, device)
+    accuracyRoot.append(accuracy_score(torch.argmax(res[0], dim=1), res[1]))
+    print('FineAll: Sentence Accuracy on test set = {}'.format(accuracyRoot[-1]))
+
+    ## FineRoot
+    checkpointFineAll = torch.load('../savedModels/d30/fineRoot/net_1000.pth')
+    net.load_state_dict(checkpointFineAll)
+    net.eval()
+    
+    res = allOutput(bank['test'], net, device)
+    accuracyAll.append(accuracy_score(torch.argmax(res[0], dim=1), res[1]))
+    print('FineRoot: Phrase Accuracy on test set = {}'.format(accuracyAll[-1]))
+
+    res = rootOutput(bank['test'], net, device)
+    accuracyRoot.append(accuracy_score(torch.argmax(res[0], dim=1), res[1]))
+    print('FineAll: Sentence Accuracy on test set = {}'.format(accuracyRoot[-1]))
+
