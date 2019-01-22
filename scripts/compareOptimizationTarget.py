ddf1f3e65fb117c688af17f4c237fecd6f069911
Gutierrez Muriedas, Juan Pedro <juanpedroghm@gmail.com>
2019-01-23 00:25:46 +0100

    Last minute fixes

--- scripts/compareOptimizationTarget.py
+++ scripts/compareOptimizationTarget.py
@@ -4,28 +4,28 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print('Training on {}'.format(device))
 
 batchSize = 512
-epochs = 1000
+epochs = 5
 
-print_every = 10
-devSet_every = 10
-save_every = 50
+print_every = 1
+devSet_every = 1
+save_every = 1
 
-saveFineAllPath = '../savedModels/d30/fineAll'
-saveFineRootPath = '../savedModels/d30/fineRoot'
+net = RNTN(device, len(word2idx), d=50)
+net.to(device)
+
+saveFineAllPath = '../savedModels/d{}/fineAll'.format(net.d)
+saveFineRootPath = '../savedModels/d{}/fineRoot'.format(net.d)
 
 totalLoss = []
 accuracyAll = []
 accuracyRoot = []
-
-net = RNTN(device, len(word2idx), d=30)
-net.to(device)
 
 loss_f = nn.NLLLoss() # Negative log likelihood loss
 optimizer = optim.Adagrad(net.parameters(), weight_decay=0.001) # Adagrad with L2 regularization
 
 ## First optimize FineAll
 
-checkpoint5000 = torch.load('../savedModels/d30/net_5000.pth')
+checkpoint5000 = torch.load('../savedModels/d{}/net_5000.pth'.format(net.d), map_location = device)
 net.load_state_dict(checkpoint5000)
 net.eval()
 
@@ -88,17 +88,15 @@ for e in range(1, epochs+1):
 
 
 ## Save date for plotting
-accuracyData = np.vstack([np.arange(10, 1001, 10), accuracyAll, accuracyRoot])
-np.savetxt("../data/accuracyComp.csv", accuracyData, delimiter=",")
+accuracyData = np.vstack([np.arange(devSet_every, epochs + 1, devSet_every), accuracyAll, accuracyRoot])
+np.savetxt("../data/d{}OptimComp.csv".format(net.d), accuracyData, delimiter=",")
 
-with torch.no_grad():
+## FineAll
+allAcc, rootAcc = getAccuracyScores(net, bank['test'], device, '../savedModels/d{}/fineAll/net_{}.pth'.format(net.d, epochs))
+print('FineAll: Phrase Accuracy on test set = {}'.format(allAcc))
+print('FineAll: Sentence Accuracy on test set = {}'.format(rootAcc))
 
-    ## FineAll
-    allAcc, rootAcc = getAccuracyScores(net, bank['test'], device, '../savedModels/d30/fineAll/net_1000.pth')
-    print('FineAll: Phrase Accuracy on test set = {}'.format(e, allAcc))
-    print('FineAll: Sentence Accuracy on test set = {}'.format(e, rootAcc))
-
-    ## FineRoot
-    allAcc, rootAcc = getAccuracyScores(net, bank['test'], device, '../savedModels/d30/fineRoot/net_1000.pth')
-    print('FineRoot: Phrase Accuracy on test set = {}'.format(e, allAcc))
-    print('FineRoot: Sentence Accuracy on test set = {}'.format(e, rootAcc))
+## FineRoot
+allAcc, rootAcc = getAccuracyScores(net, bank['test'], device, '../savedModels/d{}/fineRoot/net_{}.pth'.format(net.d, epochs))
+print('FineRoot: Phrase Accuracy on test set = {}'.format(allAcc))
+print('FineRoot: Sentence Accuracy on test set = {}'.format(rootAcc))
